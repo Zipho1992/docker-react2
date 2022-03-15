8dc328eeda106cce757582982f01adc023cf06d6
zipho-binta <zipho.binta@investec.co.za>
2022-03-15 11:38:58 +0200

    changed app text

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,14 +10,9 @@ function App() {
           Hello Zipho, starlight!
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React, maybe!
-        </a>
+        <p className="App-intro">
+          I was changed on the Feature
+        </p>
       </header>
     </div>
   );
