6975da335f916d17c80cf2d146cfa9c624b58c9d
zipho-binta <zipho.binta@investec.co.za>
2022-03-15 12:44:22 +0200

    changed app text

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Zipho, starlight!
+          Hello Zipho, starlight!!!
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p className="App-intro">
